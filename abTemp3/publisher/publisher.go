44cfe077fae6f9a32f416b4c978b425bd621496e
Eric Freeman <ericfree@badne7.ux.uis.no>
2016-02-14 13:16:42 +0100

    Renamed files.

--- abTemp3/publisher/publisher.go
+++ abTemp3/publisher/publisher.go
@@ -2,59 +2,104 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"io"
 	"time"
 
-	"github.com/ericnorway/arbitraryFailures/abTemp3/common"
 	pb "github.com/ericnorway/arbitraryFailures/abTemp3/proto"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
-type Publisher struct {
-	toBrokerChansMutex sync.RWMutex
-	toBrokerChans map[string] chan *pb.Publication
-}
-
-func NewPublisher() *Publisher {
-	return &Publisher{
-		toBrokerChans: make(map[string] chan *pb.Publication),
+func (p *Publisher) Publish(pubReq *pb.Publication) {
+	p.toBrokerChansMutex.RLock()
+	defer p.toBrokerChansMutex.RUnlock()
+	
+	for _, ch := range p.toBrokerChans {
+		ch<- pubReq
 	}
 }
 
-func main() {
-	fmt.Printf("Publisher started.\n")
+func (p *Publisher) startBrokerClients(brokerAddrs []string) {
+	for i := range brokerAddrs {
+		go p.startBrokerClient(brokerAddrs[i])
+	}
 	
-	brokerAddrs := []string{"localhost:11111", "localhost:11112", "localhost:11113", "localhost:11114"}
+	for len(p.toBrokerChans) < 3 {
+		fmt.Printf("Waiting for connections...\n")
+		time.Sleep(time.Second)
+	}
+	fmt.Printf("...done\n")
+}
+
+func (p *Publisher) startBrokerClient(brokerAddr string) bool {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
 	
-	publisher := NewPublisher()
+	conn, err := grpc.Dial(brokerAddr, opts...)
+	if err != nil {
+		fmt.Printf("Error while connecting to server: %v\n", err)
+		return false
+	}
+
+	client := pb.NewPubBrokerClient(conn)
 	
-	publisher.startBrokerClients(brokerAddrs)
+	stream, err := client.Publish(context.Background())
+	if err != nil {
+		fmt.Printf("Error while starting the Publish stream: %v\n", err)
+		return false
+	}
 	
-	time.Sleep(time.Second)
+	// Read loop
+	go func() {
+		for {
+			_, err := stream.Recv()
+			if err == io.EOF {
+				p.removeChannel(brokerAddr)
+				break		
+			}
+			if err != nil {
+				p.removeChannel(brokerAddr)
+				break
+			}
+		}
+		
+		fmt.Printf("Receive ended.\n")
+	}()
 	
-	publisher.Publish(&pb.Publication{
-		PubType: common.AB,
-		PublisherID: 1,
-		PublicationID: 1,
-		Topic: 1,
-		Content: []byte(time.Now().String()),
-	})
-	time.Sleep(time.Second)
+	ch := p.addChannel(brokerAddr)
 	
-	publisher.Publish(&pb.Publication{
-		PubType: common.AB,
-		PublisherID: 1,
-		PublicationID: 2,
-		Topic: 2,
-		Content: []byte(time.Now().String()),
-	})
-	time.Sleep(time.Second)
+	// Write loop
+	for {
+		select {
+			case req := <-ch:
+				err := stream.Send(req)
+				if err != nil {
+					p.removeChannel(brokerAddr)
+					break
+				}
+		}
+	}
 	
-	publisher.Publish(&pb.Publication{
-		PubType: common.AB,
-		PublisherID: 1,
-		PublicationID: 3,
-		Topic: 1,
-		Content: []byte(time.Now().String()),
-	})
-	time.Sleep(time.Second)
+	return true
+}
+
+func (p *Publisher) addChannel(addr string) chan *pb.Publication {
+	fmt.Printf("Broker channel to %v added.\n", addr)
+
+	p.toBrokerChansMutex.Lock()
+	defer p.toBrokerChansMutex.Unlock()
+	
+	ch := make(chan *pb.Publication, 32)
+	p.toBrokerChans[addr] = ch
+	return ch
+}
+
+func (p *Publisher) removeChannel(addr string) {
+	fmt.Printf("Broker channel to %v removed.\n", addr)
+
+	p.toBrokerChansMutex.Lock()
+	p.toBrokerChansMutex.Unlock()
+	
+	delete(p.toBrokerChans, addr)
 }
