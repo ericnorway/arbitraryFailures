26d24b389921334850150731a3f5ff1ee2a45666
Eric Freeman <ericfree@badne7.ux.uis.no>
2016-02-15 11:46:13 +0100

    Removed temporary files.

--- abTemp3/broker/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	parsedCorrectly := ParseArgs()
-	if parsedCorrectly {
-		StartBroker(*endpoint)
-	}
-}
--- abTemp3/common/common.go
+++
@@ -1,7 +0,0 @@
-package common
-
-const (
-	AB = iota
-	BRB
-	Chain
-)
