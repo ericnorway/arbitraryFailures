cbac34e51ff7339d5a256b1324958c379c97e671
Eric Freeman <ericnorway77@gmail.com>
2016-04-19 15:25:19 +0200

    Added some malicious tests.

--- perfTests/pitter/444CH_3_MAL.sh
+++ perfTests/pitter/444CH_3_MAL.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #variables
-alpha=10
-pubType=AB
+alpha=3
+pubType=Chain
 pubCount=10000
 baseResultsDir="/home/stud/ericfree/go/src/github.com/ericnorway/arbitraryFailures/results"
 resultsDir="$baseResultsDir/pitter"
 configDir="/home/stud/ericfree/go/src/github.com/ericnorway/arbitraryFailures/configs/pitter"
 brokerCount=4
-subscriberCount=1
-publisherCount=1
-maliciousPct=10
+subscriberCount=4
+publisherCount=4
+maliciousPct=25
 time=180
 
 ./test.sh $alpha $pubType $pubCount $baseResultsDir $resultsDir $configDir $brokerCount $subscriberCount $publisherCount $maliciousPct $time
